d73f17218314a4381a957639cfd13abb5dc3db5f
Tiago <tql.tql@gmail.com>
2026-07-05 03:37:16 -0300

    fix: bump versao 208 para invalidar cache SW e icones

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker para PWA - Inspetor BudGanja
-const APP_VERSION = '207';
+const APP_VERSION = '208';
 const CACHE_NAME = 'budganja-v' + APP_VERSION;
 const urlsToCache = [
     '/',
